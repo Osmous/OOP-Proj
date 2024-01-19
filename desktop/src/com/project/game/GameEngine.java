8cd935c9626894e255ff014a044349831dec2eaa
Osmous <67967981+Osmous@users.noreply.github.com>
2024-01-19 11:49:52 +0800

    initial commit

---
+++ desktop/src/com/project/game/GameEngine.java
@@ -0,0 +1,15 @@
+package com.project.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class GameEngine extends ApplicationAdapter{
+	public void create() {
+		// TODO
+	};
+	public void render() {
+		// TODO
+	};
+	public void dispose() {
+		// TODO
+	};
+}
