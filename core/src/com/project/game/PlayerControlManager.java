789de35fc5555ffd85350d0fae16b4fcfdad7787
Osmous <67967981+Osmous@users.noreply.github.com>
2024-02-17 16:10:26 +0800

    Revert "Merge pull request #5 from MysticVortex/Alisanne_PlayerControlManager"
    This reverts commit fd1c932c8ff4869bb3f2251b2e8d58ac10afd48e, reversing
    changes made to 7c8f51806fa02d1288c09a9c1ceccf05fe76bef1.

--- core/src/com/project/game/PlayerControlManager.java
+++ core/src/com/project/game/PlayerControlManager.java
@@ -1,24 +1,7 @@
 package com.project.game;
 
-// Handle player movement from IO Manager
-
 public class PlayerControlManager {
-    private Player player; // Declared at the class level
-    
-    // Constructor 
-    public PlayerControlManager(Player player) {
-        this.player = player;
-    }
-    
-    // Method to move player left
-    public void movePlayerLeft() {
-        // Implement the logic to move the player left
-        player.moveLeft();
-    }
-    
-    // Method to move player right
-    public void movePlayerRight() {
-        // Implement the logic to move the player right
-        player.moveRight();
+    public PlayerControlManager() {
+        // see gameengine render() for more like help info if u need
     }
 }
