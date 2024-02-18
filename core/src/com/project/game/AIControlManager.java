ea418eba30c7839eb103d5325e3bfe83bb2ae9c2
nabs <nnbz1723@gmail.com>
2024-02-19 01:15:14 +0800

    Update AIControlManager.java

--- core/src/com/project/game/AIControlManager.java
+++ core/src/com/project/game/AIControlManager.java
@@ -1,11 +1,50 @@
 package com.project.game;
 
+import com.project.game.Entity.EnemyEntity;
+import com.project.game.GameEngine;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
 public class AIControlManager {
-    public AIControlManager() {
-        // work with playercontrol ah. do the same thing just idk use random to make decisions or u can look into libgdx
-        // to see if theres any like exisiting ai classes. for example, making enemy home towards player when player
-        // is moving around. that one requires some form of tracking player pos and calling movement methods accordingly
+    private GameEngine gameEngine;
+    private Random random;
+
+    public AIControlManager(GameEngine gameEngine) {
+        this.gameEngine = gameEngine;
+        this.random = new Random();
+    }
+
+    public void updateAI() {
+        for (EnemyEntity enemy : gameEngine.getEntityManager().getLoadedEntity()) {
+            generateEnemyMovement(enemy);
+            generateEnemyAction(enemy);
+        }
+    }
+
+    private void generateEnemyMovement(EnemyEntity enemy) {
+        // Get player position
+        Vector2 playerPosition = gameEngine.getEntityManager().getPlayerPosition();
+
+        // Calculate direction towards the player
+        Vector2 direction = new Vector2(playerPosition.x - enemy.getPosition().x, playerPosition.y - enemy.getPosition().y);
+        direction.nor(); // Normalize the direction vector
+
+        // Adjust enemy position based on direction towards the player
+        float enemySpeed = enemy.getSpeed();
+        float deltaX = direction.x * enemySpeed;
+        float deltaY = direction.y * enemySpeed;
+
+        // Update enemy position
+        enemy.moveBy(deltaX, deltaY);
+    }
+
+    private void generateEnemyAction(EnemyEntity enemy) {
+        // Determine if the enemy should perform an attack action
+        boolean shouldAttack = random.nextBoolean(); // Randomly determine whether to attack or not
+        if (shouldAttack) {
+            // Perform attack action
+            gameEngine.attackPlayer(enemy);
+        }
     }
 }
-
-//lalalla
