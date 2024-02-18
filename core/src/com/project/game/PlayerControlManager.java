689f354d0afbe75abb5124e09e2c470c6bb5b25b
Osmous <67967981+Osmous@users.noreply.github.com>
2024-02-18 23:32:48 +0800

    Update PlayerControlManager.java

--- core/src/com/project/game/PlayerControlManager.java
+++ core/src/com/project/game/PlayerControlManager.java
@@ -1,44 +1,40 @@
 package com.project.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.project.game.Entity.PlayerEntity;
 import com.project.game.GameEngine;
 
 public class PlayerControlManager {
     // Private instances
-    private PlayerEntity player;
     private GameEngine gameEngine;
     private float playerSpeed; // Hold player's speed
 
     // Constructor
-    public PlayerControlManager(PlayerEntity player, GameEngine gameEngine, float playerSpeed) {
-        this.player = player;
+    public PlayerControlManager(GameEngine gameEngine) {
         this.gameEngine = gameEngine;
-        this.playerSpeed = playerSpeed; // Initialize player's speed
     }
 
     // Method to move player left
     private void movePlayerLeft(float delta) {
+        playerSpeed = gameEngine.entityManager.getEntitySpeed(gameEngine.entityManager.getPlayerEntityId());
         // Calculate distance to move based on player's speed and delta
         float distanceToMove = -playerSpeed * delta;
-        // Call moveLeft method
-        player.moveLeft();
         // Call updateEntity method through gameEngine
-        gameEngine.entityManager().updateEntity("moveX", player.getEntityID(), distanceToMove);
+        gameEngine.entityManager.updateEntity("moveX", gameEngine.entityManager.getPlayerEntityId(), distanceToMove);
     }
 
     // Method to move player right
     private void movePlayerRight(float delta) {
+        playerSpeed = gameEngine.entityManager.getEntitySpeed(gameEngine.entityManager.getPlayerEntityId());
         // Calculate distance to move based on player's speed and delta
         float distanceToMove = playerSpeed * delta;
-        // Call moveRight method
-        player.moveRight();
         // Call updateEntity method through gameEngine
-        gameEngine.entityManager().updateEntity("moveX", player.getEntityID(), distanceToMove);
+        gameEngine.entityManager.updateEntity("moveX", gameEngine.entityManager.getPlayerEntityId(), distanceToMove);
     }
 
     // Method to handle key input and move player accordingly
-    public void handleInput(int keyCode, float delta) {
+    public void handleInput(int keyCode) {
+        float delta = Gdx.graphics.getDeltaTime();
         switch (keyCode) {
             case Input.Keys.LEFT:
                 movePlayerLeft(delta);
