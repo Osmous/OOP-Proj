d9cc9eb9e06040c9cc4c14d9f78b17ff665135ca
Alisanne1520 <ALISANNE15.20@ichat.sp.edu.sg>
2024-02-18 23:25:34 +0800

    update code

--- core/src/com/project/game/PlayerControlManager.java
+++ core/src/com/project/game/PlayerControlManager.java
@@ -1,19 +1,19 @@
 package com.project.game;
 
 import com.badlogic.gdx.Input;
-import com.project.game.Entity.EntityManager;
 import com.project.game.Entity.PlayerEntity;
+import com.project.game.GameEngine;
 
 public class PlayerControlManager {
     // Private instances
     private PlayerEntity player;
-    private EntityManager entityManager;
+    private GameEngine gameEngine;
     private float playerSpeed; // Hold player's speed
 
     // Constructor
-    public PlayerControlManager(PlayerEntity player, EntityManager entityManager, float playerSpeed) {
+    public PlayerControlManager(PlayerEntity player, GameEngine gameEngine, float playerSpeed) {
         this.player = player;
-        this.entityManager = entityManager;
+        this.gameEngine = gameEngine;
         this.playerSpeed = playerSpeed; // Initialize player's speed
     }
 
@@ -21,18 +21,20 @@ public class PlayerControlManager {
     private void movePlayerLeft(float delta) {
         // Calculate distance to move based on player's speed and delta
         float distanceToMove = -playerSpeed * delta;
-        // Call moveLeft method and update entity's position using the updateEntity method in EntityManager
+        // Call moveLeft method
         player.moveLeft();
-        entityManager.updateEntity("moveX", player.getEntityID(), distanceToMove);
+        // Call updateEntity method through gameEngine
+        gameEngine.entityManager().updateEntity("moveX", player.getEntityID(), distanceToMove);
     }
 
     // Method to move player right
     private void movePlayerRight(float delta) {
         // Calculate distance to move based on player's speed and delta
         float distanceToMove = playerSpeed * delta;
-        // Call moveRight method and update entity's position using the updateEntity method in EntityManager
+        // Call moveRight method
         player.moveRight();
-        entityManager.updateEntity("moveX", player.getEntityID(), distanceToMove);
+        // Call updateEntity method through gameEngine
+        gameEngine.entityManager().updateEntity("moveX", player.getEntityID(), distanceToMove);
     }
 
     // Method to handle key input and move player accordingly
