246da19f8534cff15df7dd63bc13c3ef8a3e6918
Alisanne1520 <ALISANNE15.20@ichat.sp.edu.sg>
2024-02-18 22:37:39 +0800

    update player control

--- core/src/com/project/game/PlayerControlManager.java
+++ core/src/com/project/game/PlayerControlManager.java
@@ -1,7 +1,49 @@
 package com.project.game;
 
+import com.badlogic.gdx.Input;
+import com.project.game.Entity.EntityManager;
+import com.project.game.Entity.PlayerEntity;
+
 public class PlayerControlManager {
-    public PlayerControlManager() {
-        // see gameengine render() for more like help info if u need
+    // Private instances
+    private PlayerEntity player;
+    private EntityManager entityManager;
+    private float playerSpeed; // Hold player's speed
+
+    // Constructor
+    public PlayerControlManager(PlayerEntity player, EntityManager entityManager, float playerSpeed) {
+        this.player = player;
+        this.entityManager = entityManager;
+        this.playerSpeed = playerSpeed; // Initialize player's speed
+    }
+
+    // Method to move player left
+    private void movePlayerLeft(float delta) {
+        // Calculate distance to move based on player's speed and delta
+        float distanceToMove = -playerSpeed * delta;
+        // Call moveLeft method and update entity's position using the updateEntity method in EntityManager
+        player.moveLeft();
+        entityManager.updateEntity("moveX", player.getEntityID(), distanceToMove);
+    }
+
+    // Method to move player right
+    private void movePlayerRight(float delta) {
+        // Calculate distance to move based on player's speed and delta
+        float distanceToMove = playerSpeed * delta;
+        // Call moveRight method and update entity's position using the updateEntity method in EntityManager
+        player.moveRight();
+        entityManager.updateEntity("moveX", player.getEntityID(), distanceToMove);
+    }
+
+    // Method to handle key input and move player accordingly
+    public void handleInput(int keyCode, float delta) {
+        switch (keyCode) {
+            case Input.Keys.LEFT:
+                movePlayerLeft(delta);
+                break;
+            case Input.Keys.RIGHT:
+                movePlayerRight(delta);
+                break;
+        }
     }
 }
