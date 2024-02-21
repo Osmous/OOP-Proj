77840f0140b9c9ee3d06fd38043bf0861795b9d6
MysticVortex <58290494+MysticVortex@users.noreply.github.com>
2024-02-21 11:26:38 +0800

    Update IOManager.java

--- core/src/com/project/game/IOManager.java
+++ core/src/com/project/game/IOManager.java
@@ -1,42 +1,54 @@
 package com.project.game;
 
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Gdx;
+import com.project.game.Entity.Entity;
 
-public class IOManager implements InputProcessor {
-    private GameEngine gameEngine;
-    private boolean isButtonPressed;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IOManager extends InputAdapter {
+    private GameEngine gameEngine; // Reference to the game engine
+    private List<String> keypressedlist; // List to store the pressed keys
+    private boolean isButtonPressed; // Flag to track if a button is pressed
 
     public IOManager(GameEngine gameEngine) {
-        this.gameEngine = gameEngine;
-        Input.setInputProcessor(this);
-        this.isButtonPressed = false;
+        this.gameEngine = gameEngine; // Initialize the game engine
+        this.keypressedlist = new ArrayList<String>(); // Initialize the key pressed list
+        this.isButtonPressed = false; // Initialize the button pressed flag
     }
 
     @Override
     public boolean keyDown(int keycode) {
-        if (keycode == Input.Keys.LEFT) {
-            gameEngine.playerControlManager.movePlayerLeft();
-        } else if (keycode == Input.Keys.RIGHT) {
-            gameEngine.playerControlManager.movePlayerRight();
+//        if (keycode == Input.Keys.LEFT) {
+//            //gameEngine.playerControlManager.movePlayerLeft();
+//        } else if (keycode == Input.Keys.RIGHT) {
+//            //gameEngine.playerControlManager.movePlayerRight();
+//        }
+        if (this.gameEngine.simulationCycleManager.getCurrentState().equals("RUNNING")){
+            keypressedlist.add(Integer.toString(keycode));
+            System.out.println(keycode);
         }
         return true;
     }
 
     @Override
-    public boolean keyUp(int keycode) {
+    public boolean keyUp(int keycode){
+        if (this.gameEngine.simulationCycleManager.getCurrentState().equals("RUNNING")){
+            keypressedlist.remove(Integer.toString(keycode)); // Remove the released key from the list
+        }
         return true;
     }
 
-    @Override
-    public boolean keyTyped(char character) {
-        return true;
+    public List<String> getKeypressedlist() {
+        return keypressedlist; // Return the key pressed list
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         if (!isButtonPressed) {
-            isButtonPressed = true;
+            isButtonPressed = true; // Set the button pressed flag to true
             // Handle the click event here
         }
         return true;
@@ -45,9 +57,14 @@ public class IOManager implements InputProcessor {
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         if (isButtonPressed) {
-            isButtonPressed = false;
+            isButtonPressed = false; // Set the button pressed flag to false
         }
         return true;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
     }
 
     @Override
